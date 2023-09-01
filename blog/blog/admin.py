df63ed898509fa7f66fee91f333b0f0c47bac097
George Alvarado <georgealvarado305@gmail.com>
2023-09-01 06:26:08 -0400

    added models for blog post

--- blog/blog/admin.py
+++ blog/blog/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
-from .models import Carousel
+from .models import Carousel, BlogPost
 
 # Register your models here.
 admin.site.register(Carousel)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "status", "created_on")
+    list_filter = ("status",)
+    search_fields = ["title", "content"]
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(BlogPost, PostAdmin)
--- blog/blog/models.py
+++ blog/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -8,4 +9,26 @@ class Carousel(models.Model):
     sub_title = models.CharField(max_length=100)
 
     def __Str__(self):
+        return self.title
+
+
+STATUS = ((0, "Draft"), (1, "Publish"))
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    image = models.ImageField(upload_to="pics/%y/%m/%d/", blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
         return self.title
