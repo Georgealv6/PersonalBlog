e17ffaa935ef67c00c8ece2cbb0cd62903b5b3e0
George Alvarado <georgealvarado305@gmail.com>
2023-09-26 14:04:18 -0400

    commit

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -6,11 +6,11 @@ from .models import Carousel, BlogPost
 def homepage(request):
     carousel = Carousel.objects.all()
     post = BlogPost.objects.all()
-    context = {"carousel": carousel, "post": post}
+    context = {"carousel": carousel, "post_detail": post}
     return render(request, "blog/homepage.html", context)
 
 
 def detail(request):
     post = BlogPost.objects.all()
-    context = {"blog": post}
-    return render(request, context)
+    context = {"post_detail": post}
+    return render(request, "blog/post/detail.html", context)
