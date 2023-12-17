80a9510b97668f01b20d82fbfeb0da54007bce87
George Alvarado <georgealvarado305@gmail.com>
2023-12-17 17:26:41 -0500

    changed order of post

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.homepage, name="homepage"),
     path("posts/lifstyle", views.lifestyleposts, name="lifestyle"),
+    path("posts/code", views.codeposts, name="code"),
     path("post/<slug:slug>/", views.postdetail, name="post_detail"),
 ]
--- blog/blog/views.py
+++ blog/blog/views.py
@@ -4,7 +4,7 @@ from .models import Carousel, BlogPost
 
 def homepage(request):
     carousel = Carousel.objects.all()
-    post = BlogPost.objects.filter(status=1).order_by("-created_on")
+    post = BlogPost.objects.filter(status=1).order_by("created_on")
     context = {"carousel": carousel, "posts": post}
     return render(request, "blog/index.html", context)
 
@@ -20,3 +20,10 @@ def lifestyleposts(request):
     post = BlogPost.objects.filter(category=chosen_category)
     context = {"detail": post, "chosen_category": chosen_category}
     return render(request, "blog/lifestyle/lifestyle.html", context)
+
+
+def codeposts(request):
+    chosen_category = "1"
+    post = BlogPost.objects.filter(category=chosen_category)
+    context = {"detail": post, "chosen_category": chosen_category}
+    return render(request, "blog/code/codepage.html", context)
