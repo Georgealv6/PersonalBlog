5c89f2768931a55fdb36fdb529c06730436950d6
George Alvarado <georgealvarado305@gmail.com>
2024-01-26 21:00:13 -0500

    added routes to slider imgs

--- blog/blog/models.py
+++ blog/blog/models.py
@@ -1,17 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+ROUTES = (
+    (0, "homepage"),
+    (1, "lifestyle"),
+    (2, "code"),
+)
+
 
 # Create your models here.
 class Carousel(models.Model):
     image = models.ImageField(upload_to="pics/%y/%m/%d/")
     title = models.CharField(max_length=150)
     sub_title = models.CharField(max_length=100)
+    route = models.CharField(max_length=100)
 
     def __Str__(self):
         return self.title
 
 
+# Post
 STATUS = ((0, "Draft"), (1, "Publish"))
 
 GENRE = ((0, "Lifestyle"), (1, "Code"))
